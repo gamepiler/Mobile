6bfa6470657a98e77b37f2891a5c7005bd77a705
Brad Nissen <bradanissen@gmail.com>
2019-04-02 12:40:06 -0700

    Login page is created
    upcoming iteration will be better looking buttons, and make them auto-scale to the size of the device. App icon is added, colors added, logos added

--- app/src/main/java/com/bradnissen/gamepiler/MainActivity.java
+++ app/src/main/java/com/bradnissen/gamepiler/MainActivity.java
@@ -1,13 +1,35 @@
 package com.bradnissen.gamepiler;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void callTextView(View view) {
+        //get the id of the view
+        TextView tv = findViewById(R.id.textview);
+        //change the text
+        tv.setText("You clicked the login button");
+    }
+
+    public void callTextView2(View view) {
+        //get the id of the view
+        TextView tv = findViewById(R.id.textview);
+        //change the text
+        tv.setText("You clicked the Signup button");
+
+        ImageView signupButton = findViewById(R.id.signup);
+        //signupButton.setColorFilter(Color.);
     }
 }
